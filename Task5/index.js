acb0f7ddb8756377920935cdee95a05adb67b613
Amroshwayat <138685026+Amroshwayat@users.noreply.github.com>
2025-08-26 00:11:50 +0300

    Task5

---
+++ Task5/index.js
@@ -0,0 +1,18 @@
+
+
+function sum0To100Array(){
+
+    let array=[];
+    let sum=0;
+    for(var i=1;i<=100;i++){
+        array.push(i);
+    }
+    for(var i=0;i<100;i++){
+        if(array[i]%2==0){
+        sum+=array[i];
+     console.log(array[i]);}
+    }
+   
+  console.log(sum);
+
+}
